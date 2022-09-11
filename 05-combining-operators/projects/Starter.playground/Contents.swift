16091309f8fa040d050df61a47c37829179c5e74
Mate Csengeri <mate.csengeri@mondriaan.com>
2022-09-11 22:35:11 +0200

    Combining operators

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -3,7 +3,47 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "prepend(Publisher)") {
+    let publisher1 = [3,4].publisher
+    let publisher2 = [1,2].publisher
+    
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: {print($0)})
+        .store(in: &subscriptions)
+    
+}
+
+example(of: "switchToLatest") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<Int, Never>()
+    let publisher3 = PassthroughSubject<Int, Never>()
+    
+    let publishers = PassthroughSubject<PassthroughSubject<Int, Never>, Never>()
+    
+    publishers
+        .switchToLatest()
+        .sink(receiveCompletion: { _ in print("Completed!") }, receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publishers.send(publisher1)
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publishers.send(publisher2)
+    publisher1.send(3)
+    publisher2.send(4)
+    publisher2.send(5)
+    
+    publishers.send(publisher3)
+    publisher2.send(6)
+    publisher3.send(7)
+    publisher3.send(8)
+    publisher3.send(9)
+    
+    publisher3.send(completion: .finished)
+    publishers.send(completion: .finished)
+}
 
 // Copyright (c) 2021 Razeware LLC
 //
