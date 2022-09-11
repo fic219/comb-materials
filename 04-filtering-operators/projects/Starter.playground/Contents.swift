0091b232bf2dec96d6ac6a9647949a246e27c1d5
Mate Csengeri <mate.csengeri@mondriaan.com>
2022-09-11 21:30:38 +0200

    Filters

--- 04-filtering-operators/projects/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/Starter.playground/Contents.swift
@@ -3,8 +3,74 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "filter") {
+    let numbers = (1...10).publisher
+    
+    numbers
+        .filter({$0.isMultiple(of: 3)})
+        .sink(receiveValue: {print($0)})
+        .store(in: &subscriptions)
+}
 
+example(of: "removeDuplicates") {
+    let words = "hey hey there! want to listen to mister mister ?"
+        .components(separatedBy: " ")
+        .publisher
+    
+    words
+        .removeDuplicates()
+        .sink(receiveValue: {print($0)})
+        .store(in: &subscriptions)
+}
+
+
+example(of: "compactMap") {
+    // 1
+    let strings = ["a", "1.24", "3",
+                 "def", "45", "0.23"].publisher
+    // 2
+    strings
+        .compactMap { Float($0) }
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "ignoreOutput") {
+    let numbers = (1...10000).publisher
+    
+    numbers
+        .ignoreOutput()
+        .sink { print("completed with: \($0)")
+        } receiveValue: { print($0)}
+        .store(in: &subscriptions)
+}
+
+example(of: "first where") {
+    let numbers = (1...9).publisher
+    
+    numbers
+        .print()
+        .first(where: { $0 % 2 == 0})
+        .sink(receiveCompletion: { print("completed with: \($0)")}, receiveValue: {print($0)})
+        .store(in: &subscriptions)
+}
+
+example(of: "drop(untilOutputFrom:)") {
+    let isReady = PassthroughSubject<Void, Never>()
+    let taps = PassthroughSubject<Int, Never>()
+    
+    taps
+        .drop(untilOutputFrom: isReady)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    (1...5).forEach { n in
+        taps.send(n)
+    if n == 3 {
+          isReady.send()
+        }
+    }
+}
 /// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
