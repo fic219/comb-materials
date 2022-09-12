665643d6faa7ed1770cf802f04e57bed861e0348
Mate Csengeri <mate.csengeri@mondriaan.com>
2022-09-12 23:50:59 +0200

    Timers

--- 06-time-manipulation-operators/projects/Starter.playground/Pages/Delay.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Starter.playground/Pages/Delay.xcplaygroundpage/Contents.swift
@@ -2,7 +2,35 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+var subscriptions = Set<AnyCancellable>()
+
+let valuesPerSecond = 0.4
+let delayInSeconds = 1.5
+
+let sourcePublisher = PassthroughSubject<Date, Never>()
+
+let delayedPublisher = sourcePublisher.delay(for: .seconds(delayInSeconds),
+                                             scheduler: DispatchQueue.main)
+let subscription = Timer
+    .publish(every: 1.0 / valuesPerSecond, on: .main, in: .common)
+    .autoconnect()
+    .subscribe(sourcePublisher)
+
+
+let sourceTimeline = TimelineView(title: "Emitted values (\(valuesPerSecond) per sec.):")
+// 5
+let delayedTimeline = TimelineView(title: "Delayed values (with a \(delayInSeconds)s delay):")
+// 6
+let view = VStack(spacing: 50) {
+  sourceTimeline
+  delayedTimeline
+}
+// 7
+PlaygroundPage.current.liveView = UIHostingController(rootView:
+view.frame(width: 375, height: 600))
+
+sourcePublisher.displayEvents(in: sourceTimeline)
+delayedPublisher.displayEvents(in: delayedTimeline)
 
 //: [Next](@next)
 /*:
